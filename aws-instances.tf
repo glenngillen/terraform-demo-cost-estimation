9877853a4bf94280a489cbe6d357fe470884f64b
Glenn Gillen <me@glenngillen.com>
2019-08-26 12:39:19 +1000

    Shutting down services outside business hours.

--- aws-instances.tf
+++ aws-instances.tf
@@ -71,7 +71,7 @@ resource "aws_placement_group" "web" {
 resource "aws_autoscaling_group" "prod-web-servers" {
   name                      = "prod-web-servers"
   max_size                  = 11
-  min_size                  = 1
+  min_size                  = 0
   health_check_grace_period = 300
   health_check_type         = "ELB"
   desired_capacity          = 3
