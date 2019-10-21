5990459aa71e8680bf2dded9e66a0a8c26d84930
Glenn Gillen <me@glenngillen.com>
2019-10-21 17:42:47 +1100

    Add public key

--- azure.tf
+++ azure.tf
@@ -15,5 +15,5 @@ variable "password" {
 }
 
 variable "public_key" {
-  default = ""
+  default = "ssh-rsa AAAAB3NzaC1yc2EAAAABIwAAAQEA5QDJ78LFYjvka6cZcxSghrOx5jDdY3BVZLSzHbmmLtaDaGFXnUwZINbr3YzixdlSyhtxnlk/eHNEhzvlR9aFe8tHcJKRbmOETCsOY6b6LCUOC1azoGWjv9ZYsJsmMCjDRG2id90wmeynyliHnfnAWin03QUXpIvuak4DjGEQyJlkhw3H4mWVOqVowRto3cMcM/0SosgO4C1V+deN7AxnjF+O+oaRL9PRNH3Iop/UhJhp0BtfhL7JH/7UAK8iwj4x6aVmC4LqbKIIy8p4LpIgWbjYc9sVYs8yWE2s0/S3AhPNrbXAfATl9hiQ/ZqcuWtgOuDOwLd7U1iNkWj5qypzXQ== glenngillen@glenn-gillens-computer.local"
 }
