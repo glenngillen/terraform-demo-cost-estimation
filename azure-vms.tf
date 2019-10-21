2fac44f7a84cde8439809fdf024d936631026b85
Glenn Gillen <me@glenngillen.com>
2019-10-21 13:31:58 +1100

    Migrate to scaling set for prod web servers.

--- azure-vms.tf
+++ azure-vms.tf
@@ -32,7 +32,7 @@ resource "azurerm_network_interface" "main" {
   }
 }
 
-resource "azurerm_virtual_machine" "main" {
+resource "azurerm_virtual_machine_scale_set" "prod-web-servers" {
   name                  = "${var.prefix}-vm"
   location              = "${azurerm_resource_group.main.location}"
   resource_group_name   = "${azurerm_resource_group.main.name}"
