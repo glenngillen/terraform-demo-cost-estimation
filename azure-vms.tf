3c7f62863407d0e2208648347431e1f950e67f56
Glenn Gillen <me@glenngillen.com>
2019-11-04 22:00:48 +1100

    Single Azure Instance.

--- azure-vms.tf
+++ azure-vms.tf
@@ -1,6 +1,49 @@
 variable "vm_size" {
   default = "Standard_B1s"
 }
+resource "azurerm_virtual_machine" "main" {
+  name                  = "${var.prefix}-vm"
+  location              = "${azurerm_resource_group.main.location}"
+  resource_group_name   = "${azurerm_resource_group.main.name}"
+  network_interface_ids = ["${azurerm_network_interface.main.id}"]
+  vm_size               = "${var.vm_size}"
+
+  delete_os_disk_on_termination = true
+  delete_data_disks_on_termination = true
+
+  storage_image_reference {
+    publisher = "Canonical"
+    offer     = "UbuntuServer"
+    sku       = "16.04-LTS"
+    version   = "latest"
+  }
+  storage_os_disk {
+    name              = "${var.prefix}-disk"
+    caching           = "ReadWrite"
+    create_option     = "FromImage"
+    managed_disk_type = "Standard_LRS"
+  }
+  os_profile {
+    computer_name  = "hostname"
+    admin_username = "${var.username}"
+    admin_password = "${var.password}"
+
+    custom_data = "echo 'init test'"
+  }
+  os_profile_linux_config {
+    disable_password_authentication = true
+    ssh_keys {
+      key_data = "${var.public_key}"
+      path = "/home/${var.username}/.ssh/authorized_keys"
+    }
+  }
+  tags = {
+    environment = "test"
+    owner = "${var.username}"
+    organization = "hashicorp"
+    application = "example"
+  }
+}
 resource "azurerm_resource_group" "main" {
   name     = "${var.prefix}-resources"
   location = "West US"
