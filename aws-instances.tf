b4f0579ce89e460c347bc07cc45b4e1ee6aee710
Glenn Gillen <me@glenngillen.com>
2019-11-18 17:34:42 +1100

    Specify the AZs for the subnets.

--- aws-instances.tf
+++ aws-instances.tf
@@ -49,30 +49,36 @@ data "aws_ami" "ubuntu" {
   owners = ["099720109477"] # Canonical
 }
 
-#resource "aws_subnet" "us-east-1a" {
-#  vpc_id      = var.vpc_id
-#  cidr_block  = "172.31.16.0/20"
-#}
-#resource "aws_subnet" "us-east-1b" {
-#  vpc_id      = var.vpc_id
-#  cidr_block  = "172.31.32.0/20"
-#}
-#resource "aws_subnet" "us-east-1c" {
-#  vpc_id      = var.vpc_id
-#  cidr_block  = "172.31.0.0/20"
-#}
-#resource "aws_subnet" "us-east-1d" {
-#  vpc_id      = var.vpc_id
-#  cidr_block  = "172.31.64.0/20"
-#}
-#resource "aws_subnet" "us-east-1e" {
-#  vpc_id      = var.vpc_id
-#  cidr_block  = "172.31.48.0/20"
-#}
-#resource "aws_subnet" "us-east-1f" {
-#  vpc_id      = var.vpc_id
-#  cidr_block  = "172.31.80.0/20"
-#}
+resource "aws_subnet" "us-east-1a" {
+  vpc_id      = var.vpc_id
+  cidr_block  = "172.31.16.0/20"
+  availability_zone = "us-east-1a"
+}
+resource "aws_subnet" "us-east-1b" {
+  vpc_id      = var.vpc_id
+  cidr_block  = "172.31.32.0/20"
+  availability_zone = "us-east-1b"
+}
+resource "aws_subnet" "us-east-1c" {
+  vpc_id      = var.vpc_id
+  cidr_block  = "172.31.0.0/20"
+  availability_zone = "us-east-1c"
+}
+resource "aws_subnet" "us-east-1d" {
+  vpc_id      = var.vpc_id
+  cidr_block  = "172.31.64.0/20"
+  availability_zone = "us-east-1d"
+}
+resource "aws_subnet" "us-east-1e" {
+  vpc_id      = var.vpc_id
+  cidr_block  = "172.31.48.0/20"
+  availability_zone = "us-east-1e"
+}
+resource "aws_subnet" "us-east-1f" {
+  vpc_id      = var.vpc_id
+  cidr_block  = "172.31.80.0/20"
+  availability_zone = "us-east-1f"
+}
 
 resource "aws_launch_template" "web" {
   name = "web-template"
